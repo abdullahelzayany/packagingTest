12b88b86d972ec329c3fecd29634050f5c10ea79
Abdullah Elzayany <abdullah.elzayany@freiheit.com>
2021-09-27 11:52:08 +0200

    Make lowercase

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "me.abdullahelzayany"
-version = "1.0-SNAPSHOT"
+version = "1.0-snapshot"
 
 publishing {
     repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "PackagingTest"
+rootProject.name = "packagingtest"
 
